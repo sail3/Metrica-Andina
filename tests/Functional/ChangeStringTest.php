eed726314e326b8c413c146c173c28d907c5f373
Pedro Charun <mlosail3@gmail.com>
2017-07-16 16:33:16 -0500

    feat(functions test): create a test for issue

---
+++ tests/Functional/ChangeStringTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Functional;
+
+use Application\Command\ChangeString;
+
+class ChangeStringTest extends BaseTestCase {
+  public function testChangeLetters()
+  {
+    $classInstace = new ChangeString();
+    $this->assertSame('123 bcde​3', $classInstace->build("123 abcd3"));
+    $this->assertSame('**Dbtb​ 52', $classInstace->build("**Casa 52"));
+    $this->assertSame('**Dbtb​ 52A​', $classInstace->build("**Casa 52Z"));
+    // $this->assertEquals('a', "a");
+  }
+}
+// entrada : "123 abcd3" salida : "123 bcde​ 3"
+// entrada : "**Casa 52" salida : "**Dbtb​ 52"
+// entrada : "**Casa 52Z" salida : "**Dbtb​ 52A​"
