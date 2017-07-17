cd958cf944a72fe64d35bd758497fc799209df84
Pedro Charun <mlosail3@gmail.com>
2017-07-17 05:05:12 -0500

    refactor(framework test): Change templates hierarchy

--- src/routes.php
+++ src/routes.php
@@ -1,20 +1,11 @@
 <?php
 // Routes
 
-// $app->get('/[{name}]', function ($request, $response, $args) {
-//     // Sample log message
-//     $this->logger->info("Slim-Skeleton '/' route");
-//
-//     // Render index view
-//     return $this->renderer->render($response, 'index.twig', $args);
-// });
-
 $app->get('/', function ($request, $response, $args)
 {
   $jsonContent = \Application\Services\Resource::retrieveAll();
   return $this->view->render($response, 'index.twig', [
-    'page_title' => 'slim framework in action!!!!',
-    'content' => 'this content is a example of slim framework.',
+    'page_title' => 'Lista empleados',
     'employees' => $jsonContent
   ]);
 });
@@ -24,8 +15,7 @@ $app->post('/', function ($request, $response, $args)
   $params = $request->getParsedBody();
   $jsonContent = \Application\Services\Resource::findByEmail($params['search-term']);
   return $this->view->render($response, 'index.twig', [
-    'page_title' => 'slim framework in action!!!!',
-    'content' => 'this content is a example of slim framework.',
+    'page_title' => 'Lista empleados',
     'employees' => $jsonContent
   ]);
 });
